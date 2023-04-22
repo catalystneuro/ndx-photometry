be358f502b6b491400aa35b5383849302afc1afc
Ben Dichter <ben.dichter@gmail.com>
2023-04-22 18:05:02 -0400

    Update version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ except Exception:
 
 setup_args = {
     'name': 'ndx-photometry',
-    'version': '0.1.1',
+    'version': '0.2.0',
     'description': 'extension for fiber photometry data',
     'long_description': readme,
     'long_description_content_type': readme_type,
